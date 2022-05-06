8538cfc996dfe6eebc5464f17f24c024fa92e55e
David Choo <david.choo@shopee.com>
2022-05-07 01:25:57 +0800

    fix 2022-05-01 to 2022-05-02 tweet image format transition

--- updater.py
+++ updater.py
@@ -2,6 +2,7 @@ import json
 import tweepy
 # src https://towardsdatascience.com/read-text-from-image-with-one-line-of-python-code-c22ede074cac
 import cv2
+from datetime import date
 import pytesseract
 import numpy as np
 from urllib.request import urlopen
@@ -70,16 +71,6 @@ def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
   image = cv2.imdecode(arr, readFlag)
   return image
 
-def text_to_stats(text):
-  result = []
-  rgx = re.compile(r'(\d[\d,]*) +(\D+?(?:\n[a-zA-Z ]+)*)\n')
-  for match in rgx.finditer(text):
-    count, unit = match.groups()
-    count = int(count.replace(',', ''))
-    unit = unit.replace('\n', ' ')
-    result.append((count, unit))
-  return result
-
 # perc params range 0 - 1
 def crop_img(img, upper_x_perc, upper_y_perc, lower_x_perc, lower_y_perc):
   height, width, _ = img.shape
@@ -89,29 +80,46 @@ def crop_img(img, upper_x_perc, upper_y_perc, lower_x_perc, lower_y_perc):
   lower_y = round(height * lower_y_perc)
   return img[upper_y:lower_y, upper_x:lower_x]
 
-def url_to_stats(url):
-  img = url_to_image(url)
-  cropped1 = crop_img(img, 0.14, 0.22, 0.5, 0.9)
-  cropped2 = crop_img(img, 0.6, 0.22, 1, 1)
-  count_unit_list = []
+def tweet_to_images(tweet):
+  img_url = tweet.entities['media'][0]['media_url_https']
+  img = url_to_image(img_url)
+  crop_coords = [[0.14, 0.22, 0.5, 0.9], [0.6, 0.22, 1, 1]]
+  if tweet.created_at.date() > date(2022, 5, 1):
+    crop_coords = [[0.13, 0.22, 0.5, 0.9], [0.58, 0.22, 1, 1]]
+  cropped_imgs = []
+  for crop_coord in crop_coords:
+    cropped = crop_img(img, crop_coord[0], crop_coord[1], crop_coord[2], crop_coord[3])
+    cropped_imgs.append(cropped)
+  return cropped_imgs
+
+def text_to_stats(text):
+  result = []
+  rgx = re.compile(r'(\d[\d,]*)[^0-9a-zA-Z]+(\D+?(?:\n[a-zA-Z ]+)*)\n')
+  for match in rgx.finditer(text):
+    count, unit = match.groups()
+    count = int(count.replace(',', ''))
+    unit = unit.replace('\n', ' ')
+    result.append((count, unit))
+  return result
+
+def image_to_stats(img):
   # src https://pyimagesearch.com/2021/11/15/tesseract-page-segmentation-modes-psms-explained-how-to-improve-your-ocr-accuracy/
   # psm 4: Assume a single column of text of variable sizes.
-  text = pytesseract.image_to_string(cropped1, lang='eng', config='--psm 4')
-  count_unit_list += text_to_stats(text)
-  text = pytesseract.image_to_string(cropped2, lang='eng', config='--psm 4')
-  count_unit_list += text_to_stats(text)
-  return count_unit_list
+  text = pytesseract.image_to_string(img, lang='eng', config='--psm 4')
+  return text_to_stats(text)
 
 def tweet_to_stats(tweet):
-  img_url = tweet.entities['media'][0]['media_url_https']
-  return url_to_stats(img_url)
+  imgs = tweet_to_images(tweet)
+  count_unit_list = []
+  for img in imgs:
+    count_unit_list += image_to_stats(img)
+  return count_unit_list
 
 def tweets_to_data(tweets):
   data = {}
   with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
     for tweet, count_unit_list in zip(tweets, executor.map(tweet_to_stats, tweets)):
-      img_url = tweet.entities['media'][0]['media_url_https']
-      count_unit_list = url_to_stats(img_url)
+      count_unit_list = tweet_to_stats(tweet)
       data[tweet.created_at.date().isoformat()] = count_unit_list
   return data
 
@@ -162,13 +170,10 @@ def merge_units(count_unit_list, to_unit, merger_lambda, *from_units):
   count = merger_lambda(*[from_counts.get(unit, 0) for unit in from_units])
   count_unit_list.append((count, to_unit))
 
-def sort_units(count_unit_list, units):
-  count_unit_list.sort(key=lambda x: units.index(x[1]) if x[1] in units else 100)
-
 def clean_data(data):
-  unit_sequence = 'troops,planes,helicopters,tanks,artillery pieces,armored personnel carriers,MLRS,boats,vehicles,fuel tanks,UAV,anti-aircraft warfare,special equipment,mobile SRBM systems'.split(',')
+  known_units = 'troops,planes,helicopters,tanks,artillery pieces,armored personnel carriers,MLRS,boats,vehicles,fuel tanks,UAV,anti-aircraft warfare,special equipment,mobile SRBM systems,APV,boats / cutters,vehicles and fuel tanks,cruise missiles'.split(',')
   for date in data:
-    fix_approximate_units(data[date], unit_sequence)
+    fix_approximate_units(data[date], known_units)
     if date == '2022-02-26':
       data[date] = [(3500,'troops'),(14,'planes'),(8,'helicopters'),(102,'tanks'),(15,'artillery pieces'),(536,'armored personnel carriers'),(1,'BUK system')]
     elif date == '2022-03-26':
@@ -176,15 +181,34 @@ def clean_data(data):
     rename_unit(data[date], 'cars', 'vehicles')
     rename_unit(data[date], 'drones', 'UAV')
     rename_unit(data[date], 'ships/patrol boats', 'boats')
-    rename_unit(data[date], 'Hees', 'troops') # 2022-04-15 "troops" detected as "Hees"
+    rename_unit(data[date], 'Hgeps', 'troops') # 2022-04-15 "troops" detected as "Hgeps" on my laptop
+    rename_unit(data[date], 'Hees', 'troops') # 2022-04-15 "troops" detected as "Hees" on github actions
     merge_units(data[date], 'MLRS', lambda buk, grad: buk+grad, 'BUK system', 'Grad systems')
-    sort_units(data[date], unit_sequence)
+    # units changes between 05-01 - 05-02
+    rename_unit(data[date], 'armored personnel carriers', 'APV')
+    rename_unit(data[date], 'boats', 'boats / cutters')
+    merge_units(data[date], 'vehicles and fuel tanks', lambda vehicles, fuel_tanks: vehicles+fuel_tanks, 'vehicles', 'fuel tanks')
+    if date == '2022-05-01':
+      override_unit_count(data[date], 'vehicles and fuel tanks', 1796) # src https://t.co/yX3LtMGXtA
+
+def sort_units(data_per_unit, units):
+  sorted_data_per_unit = {}
+  for unit in units:
+    sorted_data_per_unit[unit] = data_per_unit[unit]
+    del data_per_unit[unit]
+  # append any remaining data of units that are not in the latest data
+  sorted_data_per_unit |= data_per_unit
+  return sorted_data_per_unit
 
 def transpose_data(data, saved_data_per_unit={}):
   for date, count_unit_list in data.items():
     for count, unit in count_unit_list:
       if all(date != saved_date for saved_date, saved_count in saved_data_per_unit.get(unit, [])):
         saved_data_per_unit[unit] = saved_data_per_unit.get(unit, []) + [[date, count]]
+  if len(data) > 1:
+    latest_date = sorted(data.keys())[-1]
+    unit_sequence = [unit for [count, unit] in data[latest_date]]
+    saved_data_per_unit = sort_units(saved_data_per_unit, unit_sequence)
   return saved_data_per_unit
 
 def process_data(data, saved_data_per_unit={}):
